8dee799ca0ca4e32adaaf9ae444e65f5d2a011c5
9young <102648020+9young@users.noreply.github.com>
2022-04-19 13:39:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function calsum(){undefined
+var a=parseint(document.getelementbyidx_x_x("num1").value);
+var b=parseint(document.getelementbyidx_x_x("num2").value);
+document.getelementbyidx_x_x("num3").value=(a+b);
+} 
+change(x) {
+return parseFloat(x);
+}
